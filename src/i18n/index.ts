2a51e75020eb5feab26bfe1162d39fb074077df2
BeastPT <ajfa2004@gmail.com>
2025-04-19 02:44:36 +0200

    feat: add locales

---
+++ src/i18n/index.ts
@@ -0,0 +1,11 @@
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+
+const i18n = new I18n({
+    en: require('./en.json'),
+    ptPT : require('./ptPT.json'),
+});
+
+i18n.locale = getLocales()[0].languageCode ?? 'en';
+
+export default i18n;
